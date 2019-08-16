09ba224ed21f70739417215f318538addb438655
actangle <674253854@qq.com>
2019-08-16 16:49:20 +0800

    创建所有实体类

---
+++ src/main/java/com/melon/o2o/entity/LocalAuth.java
@@ -0,0 +1,74 @@
+package com.melon.o2o.entity;
+
+import java.util.Date;
+
+/**
+ * @ClassName LocalAuth
+ * @Description
+ * @Author melon
+ * @Date 2019-08-16 14:53
+ * @Version
+ */
+
+public class LocalAuth {
+
+    private Long localAuthId;
+
+    private String username;
+
+    private String password;
+
+    private Date createTime;
+
+    private Date lastEditTime;
+
+    private PersonInfo personInfo;
+
+    public Long getLocalAuthId() {
+        return localAuthId;
+    }
+
+    public void setLocalAuthId(Long localAuthId) {
+        this.localAuthId = localAuthId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastEditTime() {
+        return lastEditTime;
+    }
+
+    public void setLastEditTime(Date lastEditTime) {
+        this.lastEditTime = lastEditTime;
+    }
+
+    public PersonInfo getPersonInfo() {
+        return personInfo;
+    }
+
+    public void setPersonInfo(PersonInfo personInfo) {
+        this.personInfo = personInfo;
+    }
+}
---
+++ src/main/java/com/melon/o2o/entity/ProductImg.java
@@ -0,0 +1,74 @@
+package com.melon.o2o.entity;
+
+import java.util.Date;
+
+/**
+ * @ClassName ProductImg
+ * @Description
+ * @Author melon
+ * @Date 2019-08-16 16:16
+ * @Version
+ */
+
+public class ProductImg {
+
+    private Long productImgId;
+
+    private String imgAdd;
+
+    private String imgDesc;
+
+    private Integer priority;
+
+    private Date createTime;
+
+    private Long productId;
+
+    public Long getProductImgId() {
+        return productImgId;
+    }
+
+    public void setProductImgId(Long productImgId) {
+        this.productImgId = productImgId;
+    }
+
+    public String getImgAdd() {
+        return imgAdd;
+    }
+
+    public void setImgAdd(String imgAdd) {
+        this.imgAdd = imgAdd;
+    }
+
+    public String getImgDesc() {
+        return imgDesc;
+    }
+
+    public void setImgDesc(String imgDesc) {
+        this.imgDesc = imgDesc;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+}
